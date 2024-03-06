36922f0ef5f819c9fde45d1122099e0b52f9899f
mmorabautista <mmorabautista@deloitte.com>
2024-03-06 13:09:38 -0600

    init commit

---
+++ src/main/java/guru/springframework/brewery/TssbSfgBreweryApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TssbSfgBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TssbSfgBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/brewery/TssbSfgBreweryApplicationTests.java
@@ -0,0 +1,14 @@
+package guru.springframework.brewery;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TssbSfgBreweryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
